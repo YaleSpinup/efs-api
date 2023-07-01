3a54eee70c1ec0f9f9e94e3be8ba4216213d8fc3
Brandon Tassone <brandontassone@gmail.com>
2023-06-30 20:32:39 -0400

    Fixed a bug with the KMS key retrieval

--- kms/kms.go
+++ kms/kms.go
@@ -2,9 +2,6 @@ package kms
 
 import (
 	"context"
-	"fmt"
-	"strings"
-
 	"github.com/YaleSpinup/apierror"
 	"github.com/YaleSpinup/efs-api/common"
 	"github.com/aws/aws-sdk-go/aws"
@@ -12,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/kms"
 	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 // KMS is a wrapper around the aws KMS service with some default config info
@@ -38,8 +36,8 @@ func NewSession(account common.Account) KMS {
 	return s
 }
 
-// hasInputTags given a search value check an array of keys for a match
-func (k *KMS) hasInputTags(search string, keys []string) bool {
+// isRequiredKmsTagKey given a search value check an array of keys for a match
+func (k *KMS) isRequiredKmsTagKey(search string, keys []string) bool {
 	for _, key := range keys {
 		if key == search {
 			return true
@@ -49,57 +47,81 @@ func (k *KMS) hasInputTags(search string, keys []string) bool {
 	return false
 }
 
+// isPartnerTagKey is the given tag key a partner key
+func (k *KMS) isPartnerTagKey(tagKey string) bool {
+	return strings.Split(tagKey, ":")[2] == "partner"
+}
+
+// isOrgTagKey is the given tag key an org key
+func (k *KMS) isOrgTagKey(tagKey string) bool {
+	return strings.Split(tagKey, ":")[2] == "org"
+}
+
+// GetKmsKeyIdByTags Get the required kms key id for the account by its tags
 func (k *KMS) GetKmsKeyIdByTags(ctx context.Context, inputTags []string, org string) (string, error) {
 	if len(inputTags) == 0 {
 		return "", apierror.New(apierror.ErrBadRequest, "empty kms key input tags", nil)
 	}
+	log.Info("searching for required kms key via tags...")
 
-	log.Info("retrieving list of keys to check for tags")
-
-	output, err := k.Service.ListKeysWithContext(ctx, &kms.ListKeysInput{})
+	accountKeyIds, err := k.ListKmsKeyIds(ctx)
 	if err != nil {
 		return "", err
 	}
 
-	log.Infof("retrieved %d keys", len(output.Keys))
-	log.Infof("searching for kms keys matching the tags %v", inputTags)
-
 	var targetKeyId string
-	for _, key := range output.Keys {
-		keyId := aws.StringValue(key.KeyId)
-		tags, err := k.Service.ListResourceTagsWithContext(ctx, &kms.ListResourceTagsInput{KeyId: key.KeyId})
+	for _, keyId := range accountKeyIds {
+		matchingTagsCount := 0
+		tags, err := k.GetKmsKeyTags(ctx, keyId)
 		if err != nil {
-			return "", err
+			return "", apierror.New(apierror.ErrBadRequest, "there was an error fetching the kms key tags", nil)
 		}
 
-		var foundKeys []string
-		for _, tag := range tags.Tags {
+		for _, tag := range tags {
 			tagKey := aws.StringValue(tag.TagKey)
 			tagValue := aws.StringValue(tag.TagValue)
 
-			if k.hasInputTags(tagKey, inputTags) {
-				keyValueEnd := strings.Split(tagKey, ":")[2]
-
-				if keyValueEnd != "org" || keyValueEnd == "org" && tagValue == org {
-					foundKeys = append(foundKeys, tagKey)
-				}
-
-				if len(foundKeys) == len(inputTags) {
-					targetKeyId = keyId
+			if k.isRequiredKmsTagKey(tagKey, inputTags) {
+				if (k.isOrgTagKey(tagKey) && tagValue == org) || k.isPartnerTagKey(tagKey) {
+					matchingTagsCount++
 				}
 			}
 		}
+
+		if matchingTagsCount == len(inputTags) {
+			targetKeyId = keyId
+		}
 	}
 
-	if targetKeyId == "" {
-		return "", apierror.New(
-			apierror.ErrNotFound,
-			fmt.Sprintf("cannot find kms key with tag key %s", inputTags),
-			nil,
-		)
-	}
+	log.Info("found target kms key id for the account")
 
 	return targetKeyId, nil
+}
+
+// GetKmsKeyTags get the kms key tags for a specific kms key id
+func (k *KMS) GetKmsKeyTags(ctx context.Context, keyId string) ([]*kms.Tag, error) {
+	log.Infof("fetching tags for kms key id: %s", keyId)
+	tags, _ := k.Service.ListResourceTagsWithContext(ctx, &kms.ListResourceTagsInput{
+		KeyId: aws.String(keyId),
+	})
+
+	return tags.Tags, nil
+}
+
+// ListKmsKeyIds fetch the kms key ids for an account
+func (k *KMS) ListKmsKeyIds(ctx context.Context) ([]string, error) {
+	log.Infof("fetching kms key ids for the account")
+	keysListOutput, err := k.Service.ListKeysWithContext(ctx, &kms.ListKeysInput{})
+	if err != nil {
+		return []string{}, err
+	}
+
+	var accountKeyIds []string
+	for _, key := range keysListOutput.Keys {
+		accountKeyIds = append(accountKeyIds, aws.StringValue(key.KeyId))
+	}
+
+	return accountKeyIds, nil
 }
 
 func (k *KMS) GetKmsKeyId(ctx context.Context, aliasName string) (string, error) {
